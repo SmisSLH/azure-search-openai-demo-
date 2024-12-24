e5d62de36a80bbad2663d72c27f3149aa054842e
SmisSLH <153172715+SmisSLH@users.noreply.github.com>
2024-12-24 02:54:18 +0000

    fewshotサンプルを編集した_Lee_20241224-1153

--- app/backend/approaches/chatapproach.py
+++ app/backend/approaches/chatapproach.py
@@ -10,10 +10,10 @@ from approaches.approach import Approach
 
 class ChatApproach(Approach, ABC):
     query_prompt_few_shots: list[ChatCompletionMessageParam] = [
-        {"role": "user", "content": "How did crypto do last year?"},
-        {"role": "assistant", "content": "Summarize Cryptocurrency Market Dynamics from last year"},
-        {"role": "user", "content": "What are my health plans?"},
-        {"role": "assistant", "content": "Show available health plans"},
+        {"role": "user", "content": "有給休暇の申請方法を教えてください"},
+        {"role": "assistant", "content": "有給休暇の申請方法を説明します。"},
+        {"role": "user", "content": "Shachihataの回覧機能はどんな機能ですか？"},
+        {"role": "assistant", "content": "Shachihataの回覧機能とその機能の使い方を説明します。"},
     ]
     NO_RESPONSE = "0"
 
