396ed0f4fd6e8b79d85133ffc5032ba470ed3849
SmisSLH <153172715+SmisSLH@users.noreply.github.com>
2024-12-24 04:21:24 +0000

    クエリプロンプトを編集した_Lee_20241224-1317

--- app/backend/approaches/chatapproach.py
+++ app/backend/approaches/chatapproach.py
@@ -32,7 +32,7 @@ class ChatApproach(Approach, ABC):
     Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
     Do not include any text inside [] or <<>> in the search query terms.
     Do not include any special characters like '+'.
-    If the question is not in English, translate the question to English before generating the search query.
+    The question is alwasy asked in Japanese, use Japanese to generate the search query.
     If you cannot generate a search query, return just the number 0.
     """
 
